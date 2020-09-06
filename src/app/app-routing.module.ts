447b9ba4ce778dc05100c8c73eb08aaf1a772b1b
JCln <ghmantera@gmail.com>
2020-09-06 11:11:22 +0430

    first commit for readingCounter and afew configurations added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { GuardService } from './auth/guard.service';
+import { LoginComponent } from './auth/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: AppComponent, canActivate: [GuardService] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'http://37.191.92.130'
 };
