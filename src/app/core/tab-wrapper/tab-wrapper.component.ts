609bb4237c6785aa4aa11b31f826d4539db037e9
JCln <ghmantera@gmail.com>
2020-09-08 17:09:02 +0430

    tab working for pushing and great showing now going to pop from list on close button clicked

---
+++ src/app/core/tab-wrapper/tab-wrapper.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { SidebarItemsService } from 'src/app/services/DI/sidebar-items.service';
+
+import { ISidebarItems } from './../../Interfaces/isidebar-items';
+
+@Component({
+  selector: 'app-tab-wrapper',
+  templateUrl: './tab-wrapper.component.html',
+  styleUrls: ['./tab-wrapper.component.scss']
+})
+export class TabWrapperComponent implements OnInit {
+  tabs: ISidebarItems[] = [];
+  currentRoute: ISidebarItems[];
+
+  constructor(private router: Router, private sidebarItems: SidebarItemsService) {
+    this.currentRoute = this.sidebarItems.getSideBarItems();
+  }
+
+  checkRouteStatus = () => {
+    this.router.events.subscribe(res => {
+      if (res instanceof NavigationEnd) {
+        ////// just check correct route
+        const currentRouteFound = this.currentRoute.find(items => {
+          return items.routerUrl === this.router.url
+        })
+        if (currentRouteFound) {
+          //////    //  
+          const found = this.tabs.find(item => {
+            return item.routerUrl === this.router.url
+          })
+          if (found) {
+            console.log('we have this route now !');
+            return;
+          }
+          else {
+            this.tabs.push(currentRouteFound);
+          }
+        }
+      }
+    })
+  }
+
+  
+  ngOnInit(): void {
+
+
+    this.checkRouteStatus();
+  }
+
+}
