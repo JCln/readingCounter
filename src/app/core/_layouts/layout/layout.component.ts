1ed953b273b105d78701170c5f6d9502dd3c2df5
JCln <ghmantera@gmail.com>
2020-09-15 12:12:00 +0430

    clickable sidebar with angular siblings feature working need for css configs

--- src/app/core/_layouts/layout/layout.component.ts
+++ src/app/core/_layouts/layout/layout.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class LayoutComponent {
+  sidebarStatus: boolean;
+ 
+  changeSidebarStatus($event: boolean) {
+    this.sidebarStatus = $event;
   }
 
 }
--- src/app/services/DI/sidebarItems.ts
+++ src/app/services/DI/sidebarItems.ts
@@ -1,6 +1,6 @@
 import { ISidebarItems } from './../../Interfaces/isidebar-items';
 
 export const sidebarItems: ISidebarItems[] = [
-    { routerUrl: '/wr', name: 'مدیریت کاربران', isClosable: false, isRefreshable: false, isOpenItems: false },
-    { routerUrl: '/wr/foo', name: 'گزارش گیری', isClosable: true, isRefreshable: true, isOpenItems: false }
+    { routerUrl: '/wr', name: 'مدیریت کاربران', isClosable: false, isRefreshable: false, sid_isOpenItems: false },
+    { routerUrl: '/wr/foo', name: 'گزارش گیری', isClosable: true, isRefreshable: true, sid_isOpenItems: false }
 ]
