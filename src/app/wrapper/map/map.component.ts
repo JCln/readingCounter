ae9c68ab71ae03d2c083bdba4a9d75ace7938401
JCln <ghmantera@gmail.com>
2020-09-27 00:35:07 +0330

    start again and stop before plugins fixed. at least we know version problems happened

--- src/app/wrapper/map/map.component.ts
+++ src/app/wrapper/map/map.component.ts
@@ -1,23 +1,35 @@
+import '../../../../node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.js';
+
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 
-import { test1 } from '../../../test1';
 import { MapService } from './../../services/map.service';
 
+declare let L;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit, AfterViewInit {
-
-  constructor(private mapService: MapService) { 
-    console.log(test1());
-    
+  constructor(private mapService: MapService) {
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.Routing.control({
+      waypoints: [
+        L.latLng(57.74, 11.94),
+        L.latLng(57.6792, 11.949)
+      ]
+    }).addTo(map);
+  }
   ngAfterViewInit(): void {
-    this.mapService.initMap();
+    // this.mapService.initMap();
   }
 
 }
