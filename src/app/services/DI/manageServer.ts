14406c8e7bcc305811a9991d0b4e5b8bd24fe17f
JCln <ghmantera@gmail.com>
2020-10-07 12:33:02 +0330

    manage server ui complete

--- src/app/services/DI/manageServer.ts
+++ src/app/services/DI/manageServer.ts
@@ -5,5 +5,5 @@ export const serverTasts: IManageServer[] = [
     { name: 'ریست IIS', icon: 'fa fa-repeat', background: '#F68038', color: '', },
     { name: 'ریست اپلیکیشن', icon: 'fa fa-desktop', background: '#969696', color: '', },
     { name: 'حذف خطاها', icon: 'fa fa-eraser', background: '#006c75', color: '', },
-    { name: 'آفلاین کردن اًپ', icon: '', background: '#0057a2', color: '', }
+    { name: 'آفلاین کردن اًپ', icon: 'fa fa-stop-circle', background: '#0057a2', color: '', }
 ]
