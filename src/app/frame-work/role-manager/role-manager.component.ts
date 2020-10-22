bb11776d8b38d8c82e3e77d1aaf35ad5cee7d7f0
JCln <ghmantera@gmail.com>
2020-10-22 16:20:30 +0330

    actions add

--- src/app/frame-work/role-manager/role-manager.component.ts
+++ src/app/frame-work/role-manager/role-manager.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
+import { IRoleManager } from 'src/app/Interfaces/irole-manager';
 
 import { InterfaceService } from './../../services/interface.service';
 
@@ -17,7 +19,7 @@ export class RoleManagerComponent implements OnInit {
   titleUnicodeFilter = new FormControl('');
   needDeviceIdLoginFilter = new FormControl('');
   dataSource = new MatTableDataSource();
-  columnsToDisplay = ['title', 'titleUnicode', 'needDeviceIdLogin'];
+  columnsToDisplay = ['title', 'titleUnicode', 'needDeviceIdLogin', 'actions'];
   filterValues = {
     title: '',
     id: '',
@@ -25,9 +27,53 @@ export class RoleManagerComponent implements OnInit {
     needDeviceIdLogin: ''
   };
 
-  constructor(private interfaceService: InterfaceService) {
+  constructor(private interfaceService: InterfaceService, private dialog: MatDialog) {
 
   }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  }
+
+
+
+
+  openDialog = () => {
+    return new Promise(resolve => {
+      const dialogRef = this.dialog.open(DialogContentExampleDialog);
+      dialogRef.afterClosed().subscribe(result => {
+        resolve(result)
+      });
+    });
+  }
+
+  deleteSingleRow = async (row: IRoleManager) => {
+    const dialogResult = await this.openDialog();
+    if (dialogResult) {
+      return new Promise((resolve) => {
+        this.interfaceService.deleteRole(row.id).subscribe(res => {
+          if (res) {
+            resolve(res);
+          }
+        });
+      });
+    }
+  }
+  editDialog = (row: IRoleManager) => {
+    // return new Promise(resolve => {
+    //   const dialogRef = this.dialog.open(DialogEdit, {
+    //     data: { id: this.id, title: this.title, isActive: this.isActive }
+    //   });
+    //   dialogRef.afterClosed().subscribe(result => {
+    //     console.log(result);
+
+    //     resolve(result)
+    //   });
+    // });
+  }
+
+
 
   getRole = (): any => {
     return new Promise((resolve) => {
@@ -41,7 +87,7 @@ export class RoleManagerComponent implements OnInit {
   classWrapper = async () => {
     const rolesData = await this.getRole();
     console.log(rolesData);
-    
+
     if (rolesData) {
       this.dataSource.data = rolesData;
       this.dataSource.filterPredicate = this.createFilter();
@@ -85,7 +131,7 @@ export class RoleManagerComponent implements OnInit {
       let searchTerms = JSON.parse(filter);
       return data.title.toLowerCase().indexOf(searchTerms.title) !== -1
         && data.id.toString().toLowerCase().indexOf(searchTerms.id) !== -1
-        && data.titleUnicode.toLowerCase().indexOf(searchTerms.titleUnicode) !== -1        
+        && data.titleUnicode.toLowerCase().indexOf(searchTerms.titleUnicode) !== -1
     }
     return filterFunction;
   }
