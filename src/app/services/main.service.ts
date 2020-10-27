933ddc3824067374880cd04c1d0f5c1c159443b7
JCln <ghmantera@gmail.com>
2020-10-27 16:12:25 +0330

    options not nessessary for most CRUD s

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -22,31 +22,27 @@ export class MainService {
 
   GET = (URL: string, base64?: string): Observable<any> => {
     if (base64) {
-      this.http.get(environment.API_URL + '/' + URL + '/' + base64, this.httpOptions).pipe(
+      this.http.get(environment.API_URL + '/' + URL + '/' + base64).pipe(
         retry(1)
       )
     } else {
-      return this.http.get<any>(environment.API_URL + '/' + URL, this.httpOptions).pipe(
+      return this.http.get<any>(environment.API_URL + '/' + URL).pipe(
         retry(1), // retry failed request up to 1
         // catchError(err => this.errorHandler.errorHandler(err))
       )
     }
-
-
   }
   GETID = (ID: string, URL: string, base64?: string) => {
     if (base64) {
-      this.http.get(environment.API_URL + '/' + URL + '/' + base64 + '/' + ID, this.httpOptions).pipe(
+      this.http.get(environment.API_URL + '/' + URL + '/' + base64 + '/' + ID).pipe(
         retry(1)
       )
     } else {
-      return this.http.get<any>(environment.API_URL + '/' + URL + '/' + ID, this.httpOptions).pipe(
+      return this.http.get<any>(environment.API_URL + '/' + URL + '/' + ID).pipe(
         retry(1), // retry failed request up to 1
         // catchError(err => this.errorHandler.errorHandler(err))
       )
     }
-
-
   }
   POST = (URL: string, ID?: number): any => {
     return this.http.post(environment.API_URL + '/' + URL + '/' + ID, this.httpOptions).pipe(
@@ -57,12 +53,11 @@ export class MainService {
     return this.http.post(environment.API_URL + '/' + URL, body, this.httpOptions).pipe(
       retry(1)
     );
-
   }
   PUT = (URL: string, body: object): any => {
   }
   DELETE = (URL: string, id: number): Observable<any> => {
-    return this.http.delete(environment.API_URL + '/' + URL + '/' + id, this.httpOptions).pipe(
+    return this.http.delete(environment.API_URL + '/' + URL + '/' + id).pipe(
       retry(1)
     );
   }
