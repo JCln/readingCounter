f3fa6dd5ad2ea2d5de650c0b6b4610d525c60af6
JCln <ghmantera@gmail.com>
2021-01-25 16:31:30 +0330

    afew UI, error on null

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ICredentials } from './../../Interfaces/icredentials';
+import { UtilsService } from './../../services/utils.service';
 import { AuthService } from './../auth.service';
 
 @Component({
@@ -11,12 +12,19 @@ import { AuthService } from './../auth.service';
 export class LoginComponent implements OnInit {
   userData: ICredentials = { username: '', password: '' };
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private utilsService: UtilsService
+  ) { }
 
   ngOnInit(): void {
   }
   logging = () => {
+    if (this.utilsService.isNull(this.userData.password) || this.utilsService.isNull(this.userData.username)) {
+      this.utilsService.snackBarMessageWarn('نام کاربری یا رمز عبور نمیتواند خالی باشد');
+      return;
+    }
     this.authService.logging(this.userData);
   }
-  
+
 }
