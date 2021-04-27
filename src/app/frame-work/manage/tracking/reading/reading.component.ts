8262f8680d5fa37c30142bdab76e40b261928621
JCln <ghmantera@gmail.com>
2021-04-27 16:55:23 +0430

    first commit

--- src/app/frame-work/manage/tracking/reading/reading.component.ts
+++ src/app/frame-work/manage/tracking/reading/reading.component.ts
@@ -118,5 +118,21 @@ export class ReadingComponent implements OnInit, AfterViewInit, OnDestroy {
     //restore original order
     this._selectedColumns = this._selectCols.filter(col => val.includes(col));
   }
+  fuckingPrintIt = () => {
+    var tab = document.getElementById('tab');
+    this.outputManagerService.fuckingPrintIt(tab)
+
+    // var style = "<style>";
+    // style = style + "table {width: 100%;font: 17px Calibri;}";
+    // style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    // style = style + "padding: 2px 3px;text-align: center;}";
+    // style = style + "</style>";
+
+    // var win = window.open('', '', 'height=700,width=700');
+    // win.document.write(style);          //  add the style.
+    // win.document.write(tab.outerHTML);
+    // win.document.close();
+    // win.print();
+  }
 
 }
--- src/app/services/output-manager.service.ts
+++ src/app/services/output-manager.service.ts
@@ -50,6 +50,21 @@ export class OutputManagerService {
     link.download = `${new Date().toLocaleDateString() + type}`;
     link.click();
   }
+  fuckingPrintIt = (name) => {
+    var tab = document.getElementById(name);
+
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 17px Calibri;}";
+    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "</style>";
+
+    var win = window.open('', '', 'height=700,width=700');
+    win.document.write(style);          //  add the style.
+    win.document.write(tab.outerHTML);
+    win.document.close();
+    win.print();
+  }
   exportPdf(data: any, column?: any) {
     let someTest;
     column = column._columns;
