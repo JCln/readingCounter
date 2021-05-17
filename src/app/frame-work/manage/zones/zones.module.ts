02e9c3797b01144c140723dd78f31aeb99bf7563
JCln <ghmantera@gmail.com>
2021-05-17 12:36:06 +0430

    test

---
+++ src/app/frame-work/manage/zones/zones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ZonesRoutingModule } from './zones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ZonesRoutingModule
+  ]
+})
+export class ZonesModule { }
