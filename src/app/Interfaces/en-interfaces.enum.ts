8b105a5c1d2343ecd31ac4713c69741b00617fc9
JCln <ghmantera@gmail.com>
2021-05-25 13:19:52 +0430

    uncomplete commit

---
+++ src/app/Interfaces/en-interfaces.enum.ts
@@ -0,0 +1,289 @@
+export enum ENInterfaces {
+    AuthLevel4GET = 'V1/AuthLevel4/all',
+    AuthLevel4DICTIONARY = 'V1/AuthLevel4/Dictionary',
+    AuthLevel4REMOVE = 'V1/AuthLevel4/Remove',
+    AuthLevel4EDIT = 'V1/AuthLevel4/Edit',
+    AuthLevel4ADD = 'V1/AuthLevel4/Add',
+
+    AuthLevel3GET = 'V1/AuthLevel3/all',
+    AuthLevel3DICTIONARY = 'V1/AuthLevel3/Dictionary',
+    AuthLevel3REMOVE = 'V1/AuthLevel3/Remove',
+    AuthLevel3EDIT = 'V1/AuthLevel3/Edit',
+    AuthLevel3ADD = 'V1/AuthLevel3/Add',
+
+    AuthLevel2GET = 'V1/AuthLevel2/all',
+    AuthLevel2DICTIONARY = 'V1/AuthLevel2/Dictionary',
+    AuthLevel2REMOVE = 'V1/AuthLevel2/Remove',
+    AuthLevel2EDIT = 'V1/AuthLevel2/Edit',
+    AuthLevel2ADD = 'V1/AuthLevel2/Add',
+
+    AuthLevel1GET = 'V1/AuthLevel1/all',
+    AuthLevel1DICTIONARY = 'V1/AuthLevel1/Dictionary',
+    AuthLevel1REMOVE = 'V1/AuthLevel1/Remove',
+    AuthLevel1EDIT = 'V1/AuthLevel1/Edit',
+    AuthLevel1ADD = 'V1/AuthLevel1/Add',
+
+    ZoneBoundGET = 'V1/ZoneBound/All',
+    ZoneBoundDICTIONARY = 'V1/ZoneBound/Dictionary',
+    ZoneBoundREMOVE = 'V1/ZoneBound/Remove',
+    ZoneBoundEDIT = 'V1/ZoneBound/Edit',
+    ZoneBoundADD = 'V1/ZoneBound/Add',
+
+    ZoneGET = 'V1/Zone/All',
+    ZoneDICTIONARY = 'V1/Zone/Dictionary',
+    ZoneREMOVE = 'V1/Zone/Remove',
+    ZoneEDIT = 'V1/Zone/Edit',
+    ZoneADD = 'V1/Zone/Add',
+
+    RegionGET = 'V1/Region/All',
+    RegionDICTIONARY = 'V1/Region/Dictionary',
+    RegionREMOVE = 'V1/Region/Remove',
+    RegionEDIT = 'V1/Region/Edit',
+    RegionADD = 'V1/Region/Add',
+
+    ProvinceGET = 'V1/Province/All',
+    ProvinceDICTIONARY = 'V1/Province/Dictionary',
+    ProvinceREMOVE = 'V1/Province/Remove',
+    ProvinceEDIT = 'V1/Province/Edit',
+    ProvinceADD = 'V1/Province/Add',
+
+    CountryGET = 'V1/Country/All',
+    CountryDICTIONARY = 'V1/Country/Dictionary',
+    CountryREMOVE = 'V1/Country/Remove',
+    CountryEDIT = 'V1/Country/Edit',
+    CountryADD = 'V1/Country/Add',
+
+    RoleGET = 'V1/Role/All',
+    RoleDICTIONARY = 'V1/Role/Dictionary',
+    RoleREMOVE = 'Role/Remove',
+    RoleEDIT = 'Role/Edit',
+    RoleADD = 'V1/Role/Add',
+
+    downloadFileGET = 'V1/Download/File',
+    downloadFileInfo = 'V1/Download/File/info',
+    downloadFileForbidden = 'V1/Download/File/forbidden',
+
+    ReadingConfigALL = 'V1/ReadingConfigDefault/All',
+    ReadingConfigGET = 'V1/ReadingConfigDefault/Get',
+    ReadingConfigREMOVE = 'V1/ReadingConfigDefault/Remove',
+    ReadingConfigEDIT = 'V1/ReadingConfigDefault/Edit',
+    ReadingConfigADD = 'V1/ReadingConfigDefault/Add',
+
+    FormulaWaterAll = 'V1/AbBahaFormula/All',
+    FormulaWaterEdit = 'V1/AbBahaFormula/Edit',
+    FormulaWaterExcelSample = 'v1/abbahaformula/excelsample',
+    FormulaWaterAdd = 'V1/AbBahaFormula/Add',
+    FormulaWaterAddExcel = 'V1/AbBahaFormula/AddExcel',
+    FormulaWaterRemove = 'V1/AbBahaFormula/Remove',
+
+    FormulaBudgetAll = 'V1/BudgetFormula/All',
+    FormulaBudgetEdit = 'V1/BudgetFormula/Edit',
+    FormulaBudgetExcelSample = 'V1/BudgetFormula/ExcelSample',
+    FormulaBudgetAdd = 'V1/BudgetFormula/Add',
+    FormulaBudgetAddExcel = 'V1/BudgetFormula/AddExcel',
+    FormulaBudgetRemove = 'V1/BudgetFormula/Remove',
+
+    FormulaTabsare2All = 'V1/Tabsare2Formula/All',
+    FormulaTabsare2Edit = 'V1/Tabsare2Formula/Edit',
+    FormulaTabsare2Add = 'V1/Tabsare2Formula/Add',
+    FormulaTabsare2Remove = 'V1/Tabsare2Formula/Remove',
+
+    FormulaTabsare3All = 'V1/Tabsare3Formula/All',
+    FormulaTabsare3ExcelSample = 'V1/Tabsare3Formula/ExcelSample',
+    FormulaTabsare3Edit = 'V1/Tabsare3Formula/Edit',
+    FormulaTabsare3Add = 'V1/Tabsare3Formula/Add',
+    FormulaTabsare3AddExcel = 'V1/Tabsare3Formula/AddExcel',
+    FormulaTabsare3Remove = 'V1/Tabsare3Formula/Remove',
+    
+    
+All =  V1/TextOutputField/All
+All =  V1/User/All
+V1 =  uid, 'V1
+V1 =  UID, 'V1
+ResetPassword =  V1/User/ResetPassword
+Activate =  V1/User/Activate
+Deactivate =  V1/User/Deactivate
+FragmentMaster =  V1/Fragment/Master
+FragmentMaster =  V1/Fragment/Master
+FragmentMaster =  V1/Fragment/Master
+FragmentMaster =  V1/Fragment/Master
+FragmentMaster =  V1/Fragment/Master
+FragmentV1 =  asterId, 'V1
+FragmentDetials =  V1/Fragment/Detials
+FragmentDetails =  V1/Fragment/Details
+FragmentDetails =  V1/Fragment/Details
+CounterReaders =  V1/User/CounterReaders
+UserEdit =  V1/user/Edit
+UserAdd =  V1/user/Add
+UserAdd =  V1/User/Add
+APKPreList =  V1/APK/PreList
+APKLast =  V1/APK/Last
+APKUpload =  V1/APK/Upload
+OutputDbf =  V1/Output/Dbf
+Outputsingle =  V1/Output/single
+
+ForbiddenByParam =  V1/Forbidden/ByParam
+trackingImported =  V1/Tracking/Imported
+trackingLoaded =  V1/Tracking/Loaded
+trackingReading =  V1/Tracking/Reading
+trackingOffloaded =  V1/Tracking/Offloaded
+trackingFinished =  V1/Tracking/Finished
+trackingLastStates =  V1/Tracking/LastStates
+trackingEdit =  V1/Tracking/Edit
+trackingToImported =  V1/Tracking/ToImported
+trackingToReading =  V1/Tracking/ToReading
+trackingToOffloaded =  V1​/Tracking​/ToOffloaded
+trackingPre =  V1/Tracking/Pre
+trackingFinishReading =  V1/Tracking/FinishReading
+trackingRemove =  V1/Tracking/Remove
+trackingFollowUp =  V1/Tracking/FollowUp
+Search =  V1/List/Search
+Master =  V1/ReadingReport/Master
+Details =  V1/ReadingReport/Details
+Offloaded =  V1/List/Offloaded
+Offloaded =  V1/List/Offloaded
+Offloaded =  V1/List/Offloaded
+Offloaded =  V1/List/Offloaded
+Offloaded =  V1/List/Offloaded
+Analyze =  V1/Tracking/Analyze
+TraverseDifferential =  V1/List/TraverseDifferential
+Offloaded =  V1/List/Offloaded
+Offloaded =  V1/List/Offloaded
+Offloaded =  V1/List/Offloaded
+Offloaded =  V1/List/Offloaded
+OffLoaded =  V1/List/OffLoaded
+OffLoaded =  V1/List/OffLoaded
+OffLoaded =  V1/List/OffLoaded
+All =  V1/readingPeriod/All
+Dictionary =  V1/readingPeriod/Dictionary
+DictionaryByKind =  V1/ReadingPeriod/DictionaryByKind
+Dictionary =  V1/readingPeriod/Dictionary
+Edit =  V1/readingPeriod/Edit
+Add =  V1/readingPeriod/Add
+Remove =  V1/readingPeriod/Remove
+All =  V1/readingPeriodKind/All
+Dictionary =  V1/readingPeriodKind/Dictionary
+Edit =  V1/readingPeriodKind/Edit
+Add =  V1/readingPeriodKind/Add
+Remove =  V1/readingPeriodKind/Remove
+All =  V1/Karbari/All
+Dictionary =  V1/Karbari/Dictionary
+DictionaryCode =  V1/Karbari/DictionaryCode
+Remove =  V1/Karbari/Remove
+Edit =  V1/Karbari/Edit
+Add =  V1/Karbari/Add
+All =  V1/CounterState/All
+Dictionary =  V1/CounterState/Dictionary
+DictionaryByZoneId =  V1/CounterState/DictionaryByZoneId
+DictionaryBycode =  V1/CounterState/DictionaryBycode
+GridFriendly =  V1/CounterState/GridFriendly
+Remove =  V1/CounterState/Remove
+Edit =  V1/CounterState/Edit
+Add =  V1/CounterState/Add
+All =  V1/CounterReport/All
+Get =  V1/CounterReport/Get
+Remove =  V1/CounterReport/Remove
+Edit =  V1/CounterReport/Edit
+Add =  V1/CounterReport/Add
+All =  V1/Qotr/All
+Dictionary =  V1/Qotr/Dictionary
+Remove =  V1/Qotr/Remove
+Edit =  V1/Qotr/Edit
+Add =  V1/Qotr/Add
+    
+
+
+this.mainService.GET('V1/TextOutputField/All');
+this.mainService.GET('V1/User/All');
+this.mainService.GETID(uuid, 'V1/user/Edit');
+this.mainService.GETID(UUID, 'V1/User/Logins');
+this.mainService.POSTSG('V1/User/ResetPassword', UUID);
+this.mainService.POSTSG('V1/User/Activate', UUID);
+this.mainService.POSTSG('V1/User/Deactivate', UUID);
+this.mainService.GET('V1/Fragment/Master/All');
+this.mainService.POSTBODY('V1/Fragment/Master/Remove', body);
+this.mainService.POSTBODY('V1/Fragment/Master/Edit', body);
+this.mainService.POSTBODY('V1/Fragment/Master/Add', body);
+this.mainService.POSTBODY('V1/Fragment/Master/Validate', body);
+this.mainService.GETID(masterId, 'V1/Fragment/Details');
+this.mainService.POSTBODY('V1/Fragment/Detials/Remove', body);
+this.mainService.POSTBODY('V1/Fragment/Details/Edit', body);
+this.mainService.POSTBODY('V1/Fragment/Details/Add', body);
+this.mainService.GET(`V1/User/CounterReaders/${zoneId}`);
+this.mainService.POSTBODY('V1/user/Edit', body);
+this.mainService.GET('V1/user/Add');
+this.mainService.POSTBODY('V1/User/Add', body);
+this.mainService.GET('V1/APK/PreList');
+this.mainService.GET('V1/APK/Last');
+this.mainService.POSTBODY('V1/APK/Upload', body);
+this.mainService.POSTBLOB('V1/Output/Dbf', body);
+this.mainService.POSTBLOB('V1/Output/single', body);
+this.mainService.POSTBODY('V1/Forbidden/ByParam', body);
+this.mainService.GET('V1/Tracking/Imported');
+this.mainService.GET('V1/Tracking/Loaded');
+this.mainService.GET('V1/Tracking/Reading');
+this.mainService.GET('V1/Tracking/Offloaded');
+this.mainService.GET('V1/Tracking/Finished');
+this.mainService.GET('V1/Tracking/LastStates');
+this.mainService.POSTBODY('V1/Tracking/Edit', body);
+this.mainService.POSTBODY('V1/Tracking/ToImported', body);
+this.mainService.POSTBODY('V1/Tracking/ToReading', body);
+this.mainService.POSTBODY('V1​/Tracking​/ToOffloaded', body);
+this.mainService.POSTBODY('V1/Tracking/Pre', body);
+this.mainService.POSTBODY('V1/Tracking/FinishReading', body);
+this.mainService.POSTBODY('V1/Tracking/Remove', body);
+this.mainService.GET(`V1/Tracking/FollowUp/?trackNumber=${trackNumber}`);
+this.mainService.POSTBODY('V1/List/Search/Moshtarak', body);
+this.mainService.POSTBODY('V1/ReadingReport/Master/WithParam', body);
+this.mainService.POSTBODY('V1/ReadingReport/Details/WithParam', body);
+this.mainService.POSTBODY('V1/List/Offloaded/Traverse', body);
+this.mainService.POSTBODY('V1/List/Offloaded/TraverseDifferential', body);
+this.mainService.POSTBODY('V1/List/Offloaded/Karkard', body);
+this.mainService.POSTBODY('V1/List/Offloaded/KarkardChart', body);
+this.mainService.POSTBODY('V1/List/Offloaded/TraverseDifferntialChart', body);
+this.mainService.POSTBODY('V1/Tracking/Analyze/ByParam', body);
+this.mainService.GET('V1/List/TraverseDifferential/Dictionary');
+this.mainService.POSTBODY('V1/List/Offloaded/KarkardDaily', body);
+this.mainService.POSTBODY('V1/List/Offloaded/Gis', body);
+this.mainService.POSTBODY('V1/List/Offloaded/DispersalHours', body);
+this.mainService.POSTBODY('V1/List/Offloaded/DispersalChart', body);
+this.mainService.GET(`V1/List/OffLoaded/All/${trackingId}`);
+this.mainService.GET(`V1/List/OffLoaded/PerDay/${trackNumber}`);
+this.mainService.POSTBODY('V1/List/OffLoaded/PerDayXY', body);
+this.mainService.GET('V1/readingPeriod/All');
+this.mainService.GET(`V1/readingPeriod/Dictionary/${zoneId}`);
+this.mainService.GET(`V1/ReadingPeriod/DictionaryByKind/${kindId}`);
+this.mainService.GET(`V1/readingPeriod/Dictionary/${zoneId}/${kindId}`);
+this.mainService.POSTBODY('V1/readingPeriod/Edit', body);
+this.mainService.POSTBODY('V1/readingPeriod/Add', body);
+this.mainService.POST('V1/readingPeriod/Remove', id);
+this.mainService.GET('V1/readingPeriodKind/All');
+this.mainService.GET('V1/readingPeriodKind/Dictionary');
+this.mainService.POSTBODY('V1/readingPeriodKind/Edit', body);
+this.mainService.POSTBODY('V1/readingPeriodKind/Add', body);
+this.mainService.POST('V1/readingPeriodKind/Remove', id);
+this.mainService.GET('V1/Karbari/All');
+this.mainService.GET('V1/Karbari/Dictionary');
+this.mainService.GET('V1/Karbari/DictionaryCode');
+this.mainService.POST('V1/Karbari/Remove', id);
+this.mainService.POSTBODY('V1/Karbari/Edit', body);
+this.mainService.POSTBODY('V1/Karbari/Add', body);
+this.mainService.GET('V1/CounterState/All');
+this.mainService.GET('V1/CounterState/Dictionary');
+this.mainService.GET(`V1/CounterState/DictionaryByZoneId/${zoneId}`);
+this.mainService.GET(`V1/CounterState/DictionaryBycode/${zoneId}`);
+this.mainService.POSTBODY('V1/CounterState/GridFriendly', body);
+this.mainService.POST('V1/CounterState/Remove', id);
+this.mainService.POSTBODY('V1/CounterState/Edit', body);
+this.mainService.POSTBODY('V1/CounterState/Add', body);
+this.mainService.GET('V1/CounterReport/All');
+this.mainService.GET('V1/CounterReport/Get');
+this.mainService.POST('V1/CounterReport/Remove', id);
+this.mainService.POSTBODY('V1/CounterReport/Edit', body);
+this.mainService.POSTBODY('V1/CounterReport/Add', body);
+this.mainService.GET('V1/Qotr/All');
+this.mainService.GET('V1/Qotr/Dictionary');
+this.mainService.POST('V1/Qotr/Remove', id);
+this.mainService.POSTBODY('V1/Qotr/Edit', body);
+this.mainService.POSTBODY('V1/Qotr/Add', body);
+}
