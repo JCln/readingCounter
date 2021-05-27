d4af8e2ce5db20ba76798d77b95fb94aa001e749
JCln <ghmantera@gmail.com>
2021-05-27 12:57:06 +0430

    env done

--- src/assets/env.js
+++ src/assets/env.js
@@ -11,4 +11,5 @@
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
     window.__env.enableDebug = true;
+    window.__env.enableENV = true;
 }(this));
