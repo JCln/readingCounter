3c5ecb60d0dd8fa4fc4c02c88d417295ad2f04c8
JCln <ghmantera@gmail.com>
2021-05-30 18:26:28 +0430

    auth guard is only on app rout

--- src/app/frame-work/frame-work-routing.module.ts
+++ src/app/frame-work/frame-work-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { GuardService } from '../auth/guard.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', canActivate: [GuardService], loadChildren: () => import('./map/map.module').then(mapComponent => mapComponent.MapModule) },
-  { path: 'imd', canActivate: [GuardService], loadChildren: () => import('./import-dynamic/import-dynamic.module').then(importDynamicDto => importDynamicDto.ImportDynamicModule) },
-  { path: 'profile', canActivate: [GuardService], loadChildren: () => import('./profile/profile.module').then(profile => profile.ProfileModule) },
-  { path: 'msge', canActivate: [GuardService], loadChildren: () => import('./messages/messages.module').then(msge => msge.MessagesModule) },
-  { path: 'privacy', canActivate: [GuardService], loadChildren: () => import('./privacy/privacy.module').then(pr => pr.PrivacyModule) },
-  { path: 'rpts', canActivate: [GuardService], loadChildren: () => import('./reports/reports.module').then(reports => reports.ReportsModule) },
-  { path: 'm', canActivate: [GuardService], loadChildren: () => import('./manage/manage.module').then(m => m.ManageModule) },
-  { path: 'mu', canActivate: [GuardService], loadChildren: () => import('./user-manager/user-manager.module').then(userManager => userManager.UserManagerModule) },
+  { path: '', loadChildren: () => import('./map/map.module').then(mapComponent => mapComponent.MapModule) },
+  { path: 'imd', loadChildren: () => import('./import-dynamic/import-dynamic.module').then(importDynamicDto => importDynamicDto.ImportDynamicModule) },
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then(profile => profile.ProfileModule) },
+  { path: 'msge', loadChildren: () => import('./messages/messages.module').then(msge => msge.MessagesModule) },
+  { path: 'privacy', loadChildren: () => import('./privacy/privacy.module').then(pr => pr.PrivacyModule) },
+  { path: 'rpts', loadChildren: () => import('./reports/reports.module').then(reports => reports.ReportsModule) },
+  { path: 'm', loadChildren: () => import('./manage/manage.module').then(m => m.ManageModule) },
+  { path: 'mu', loadChildren: () => import('./user-manager/user-manager.module').then(userManager => userManager.UserManagerModule) },
   { path: 'pnf', component: PageNotFoundComponent },
   { path: '**', redirectTo: 'pnf', pathMatch: 'full' }
 ];
