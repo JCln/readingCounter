e6f7caa0276e6840cbd1360f0c11a59ec27084c7
JCln <ghmantera@gmail.com>
2021-07-21 15:01:47 +0430

    change background image on different modes

--- src/app/frame-work/frame-work.component.ts
+++ src/app/frame-work/frame-work.component.ts
@@ -4,6 +4,7 @@ import '../../../src/assets/L.EasyButton/src/easy-button.js';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HelpWrapperService } from 'services/help-wrapper.service';
+import { ThemeService } from 'services/theme.service';
 
 @Component({
   selector: 'app-frame-work',
@@ -14,8 +15,11 @@ export class FrameWorkComponent implements OnInit {
   @Input() pageTitle: string = '';
   @Input() refreshPage: boolean;
   orderId;
-
-  constructor(private route: ActivatedRoute, private helpWrapperService: HelpWrapperService) {
+  constructor(
+    private route: ActivatedRoute,
+    private helpWrapperService: HelpWrapperService,
+    public themeService: ThemeService
+  ) {
   }
   ngOnInit(): void {
     this.orderId = this.route.snapshot.paramMap.get('isShowMap');
