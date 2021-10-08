4ab09dfe1cf0241fa838bfcfad38fa139e296e31
JCln <ghmantera@gmail.com>
2021-10-08 17:46:15 +0330

    new version

--- src/app/services/DI/info-version.ts
+++ src/app/services/DI/info-version.ts
@@ -3,14 +3,14 @@ import { IDictionaryManager } from 'interfaces/ioverall-config';
 export const infoVersionItems: IDictionaryManager[] = [    
     {
         id: '2',
-        title: 'بهبود عملکرد برنامه',
+        title: 'داشبورد با امکانات جدید',
         isSelected: true
-    },
+    },    
     {
         id: '2',
-        title: 'رفع ایرادات جزئی',
+        title: 'بهبود عملکرد برنامه',
         isSelected: true
-    },
+    },    
 ]
 export class infoVersion {
 
--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -4,7 +4,7 @@ import { ActivatedProvince } from '../classes/activated-province';
 
 export class EnvService implements IENV {
 
-  temp = ActivatedProvince.DEFAULT;
+  temp = ActivatedProvince.ESF;
 
   headerProvinceTitle = this.temp.headerProvinceTitle;
   API_URL = this.temp.API_URL;
