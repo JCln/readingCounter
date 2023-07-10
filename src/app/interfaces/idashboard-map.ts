7d0b9497d4f502d0a8dbd33a53d3c987b32e7946
JCln <ghmantera@gmail.com>
2023-07-10 21:40:10 +0430

    pull from remote

---
+++ src/app/interfaces/idashboard-map.ts
@@ -0,0 +1,130 @@
+export interface IAnalyzeRes {
+    zoneId: number,
+    regionTitle: string,
+    zoneTitle: string,
+    caption: string,
+    statusTitle: string,
+    min: number,
+    max: number,
+    average: number,
+    variance: number,
+    standardDeviation: number,
+    median: number,
+    mode: number,
+    duration: string,
+    timeSpan: {
+        ticks: number,
+        days: number,
+        hours: number,
+        milliseconds: number,
+        minutes: number,
+        seconds: number,
+        totalDays: number,
+        totalHours: number,
+        totalMilliseconds: number,
+        totalMinutes: number,
+        totalSeconds: number
+    }
+}
+export interface IDashboardUsersInfo {
+    oldPassword: string,
+    newPassword: string,
+    confirmPassword: string
+}
+export interface IDashboardKarkardTimed {
+    adiCount: number,
+    faqedCount: number,
+    maneCount: number,
+    xarabCount: number,
+    tavizCount: number,
+    saierCount: number,
+    caption: string
+}
+export interface IDashboardUnReadCount {
+    value: number,
+    title: string,
+    order: number
+}
+export interface IDashboardMoshtarakCount {
+    value: number,
+    title: string,
+    order: number
+}
+export interface IDashboardEditCountConfig {
+    unEditCount: number,
+    editedCount: number,
+    ratio: number
+}
+export interface IDashboardEditCount {
+    daily: IDashboardEditCountConfig,
+    weekly: IDashboardEditCountConfig,
+    monthly: IDashboardEditCountConfig,
+    yearly: IDashboardEditCountConfig,
+}
+export interface IDashboardDateDifferenceConfig {
+    count: number,
+    median: number,
+    average: number,
+    standardDeviation: number,
+    variance: number,
+    mode: number
+}
+export interface IDashboardDateDifference {
+    yesterday: IDashboardDateDifferenceConfig,
+    weekly: IDashboardDateDifferenceConfig,
+    monthly: IDashboardDateDifferenceConfig,
+    annualy: IDashboardDateDifferenceConfig,
+}
+export interface IDashboardTimed {
+    inDayCount: number,
+    inWeekCount: number,
+    inMonthCount: number,
+    inYearCount: number
+}
+export interface IDashboardReadDaily {
+    count: number,
+    period: string,
+    hint: string
+}
+export interface IDashboardTraverseTimed {
+    ahad: IDashboardTimed,
+    karbari: IDashboardTimed,
+    mobile: IDashboardTimed,
+    counterSerial: IDashboardTimed,
+    empty: IDashboardTimed,
+}
+export interface IDashboardTimedItem {
+    _8_10: number,
+    _8_10Closed: number,
+    _8_10Rate: number,
+    _10_12: number,
+    _10_12Closed: number,
+    _10_12Rate: number,
+    _12_14: number,
+    _12_14Closed: number,
+    _12_14Rate: number,
+    _14_16: number,
+    _14_16Closed: number,
+    _14_16Rate: number,
+    _16_18: number,
+    _16_18Closed: number,
+    _16_18Rate: number,
+    other: number,
+    otherClosed: number,
+    otherRate: number,
+    title: string
+}
+export interface IDashboardSpecial {
+    daily: IDashboardTimedItem,
+    weekly: IDashboardTimedItem,
+    monthly: IDashboardTimedItem,
+    yearly: IDashboardTimedItem
+}
+export interface IGisXYResponse {
+    x: string,
+    y: string,
+    gisAccuracy: string,
+    info1: string,
+    info2: string,
+    info3: string
+}
---
+++ src/app/interfaces/ireads-manager.ts
@@ -0,0 +1,226 @@
+/* READING MANAGER */
+export interface IKarbari {
+    id: number,
+    moshtarakinId: number,
+    title: string,
+    provinceId: number | string,
+    isMaskooni: boolean,
+    isTejari: boolean,
+    isSaxt: boolean,
+    hasReadingVibrate: boolean
+}
+// export interface IQotr {
+
+// }
+export interface IReadingPeriod {
+    id: number,
+    title: string,
+    moshtarakinId: number,
+    readingPeriodKindId: number | string,
+    zoneId: number | string,
+    clientOrder: number
+}
+export interface IReadingPeriodKind {
+    id: number,
+    title: string,
+    moshtarakinId: number,
+    clientOrder: number,
+    isEnabled: boolean
+}
+export interface ICounterReport {
+    id: number,
+    moshtarakinId: number,
+    title: string,
+    zoneId: number | string,
+    isAhad: boolean,
+    isKarbari: boolean,
+    canNumberBeLessThanPre: boolean,
+    isTavizi: boolean,
+    clientOrder: number,
+    hasImage: boolean
+}
+
+// fragment manager 
+export interface IFragmentDetailsByEshterakReq {
+    fromEshterak: string,
+    toEshterak: string,
+    zoneId: number
+}
+export interface IAutomaticImportAddEdit {
+    id?: string,
+    fragmentMasterId: string,
+    readingPeriodKindId: number,
+    startDay: string,
+    endDay: string,
+    startTime: string,
+    counterReaderId: string,
+    alalHesabPercent: number,
+    imagePercent: number,
+    hasPreNumber: boolean,
+    displayBillId: boolean,
+    displayRadif: boolean
+}
+export interface IAutomaticImport {
+    id: string,
+    fragmentMasterId: string,
+    readingPeriodId: number,
+    jobDescription: string,
+    dueDateTime: string,
+    dueJalaliDay: string,
+    dueTime: string,
+    delayedJobId: string,
+    executionCount: number,
+    executionResult: string,
+    successExecution: boolean,
+    isActive: boolean,
+    createDateTime: string
+}
+export interface IFragmentMaster {
+    id?: string,
+    zoneId: any,
+    zoneTempId?: number,
+    routeTitle: string,
+    fromEshterak: string,
+    toEshterak: string,
+    isValidated?: boolean,
+    isNew?: boolean
+}
+export interface IFragmentDetails {
+    id?: string,
+    fragmentMasterId: string,
+    routeTitle: string,
+    fromEshterak: string,
+    toEshterak: string,
+    orderDigit?: number,
+    orderPersian?: string,
+    isNew?: boolean
+    // for batch
+    trackNumber?: number,
+    count?: number,
+    counterReaderName?: string
+}
+export interface IImageAttribution {
+    id?: number,
+    title: string,
+    isNew?: boolean
+}
+export interface IGuild {
+    id: number,
+    title: string,
+    moshtarakinId: number,
+    isActive?: boolean,
+    isNew?: boolean
+}
+export interface IDynamicTraverse {
+    id: number,
+    title: string,
+    storageTitle: string,
+    isChangeable: boolean,
+    defaultValue: boolean,
+    isActive?: boolean,
+    isNew?: boolean
+}
+export interface ICounterState {
+    id?: number,
+    moshtarakinId: number,
+    title: string,
+    zoneId: number,
+    clientOrder: number,
+    canEnterNumber: boolean,
+    isMane: boolean,
+    canNumberBeLessThanPre: boolean,
+    isTavizi: boolean,
+    hasImage: boolean,
+    shouldEnterNumber: boolean,
+    isXarab: boolean,
+    isFaqed: boolean,
+    isNew?: boolean
+}
+// export interface IGridFriendlyFilter {
+//     field: string,
+//     operator: string,
+//     logic: string,
+//     filter: [
+//         IGridFriendlyFilter
+//     ] | null
+// }
+export interface ICounterStateGridFriendlyReq {
+    take: number,
+    skip: number,
+    sort: [
+        {
+            field: string,
+            dir: string
+        }
+    ],
+    filter: {
+        // field: string,
+        // operator: string,
+        logic: string,
+        filters: any | null
+    }
+    ,
+    group: [
+        {
+            field: string,
+            dir: string,
+            aggregates: [
+                {
+                    field: string,
+                    aggregate: string
+                }
+            ]
+        }
+    ],
+    aggregate: [
+        {
+            field: string,
+            aggregate: string
+        }
+    ]
+}
+export interface ICounterStateGridFriendlyResp {
+    aggregates: null;
+    data: ICounterState[];
+    errors: null;
+    groups: null;
+    total: number;
+}
+export interface IReadingPeriodKind {
+    id: number,
+    title: string,
+    moshtarakinId: number,
+    readingPeriodKindId: number,
+    zoneId: number | string,
+    clientOrder: number,
+    days: number
+}
+// ab baha formular manager
+export interface IAbBahaFormula {
+    id: string,
+    zoneId: number | string,
+    karbariMoshtarakinCode: number | string,
+    fromDate: string,
+    toDate: string,
+    fromRate: number,
+    toRate: number,
+    abFormula: string,
+    fazelabFormula: string,
+    formula?: string
+}
+export interface ITabsare2Formula {
+    id: string,
+    zoneId: number | string,
+    formula: string
+}
+// 
+export interface ITextOutput {
+    id?: any,
+    columnId?: number,
+    zoneId: number | string,
+    itemTitle: string,
+    startIndex: any,
+    endIndex: any,
+    length: any,
+    isNew?: boolean
+}
