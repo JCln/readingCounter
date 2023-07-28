fd12582695db0a679b63c83fb1ac2c545bb10543
JCln <ghmantera@gmail.com>
2023-07-28 23:46:41 +0430

    14020506

--- src/app/frame-work/offline-mode/single-reading-counter/single-reading-counter.component.ts
+++ src/app/frame-work/offline-mode/single-reading-counter/single-reading-counter.component.ts
@@ -1,29 +1,37 @@
 import { OfflineModeService } from 'services/offline-mode.service';
 import { CloseTabService } from 'services/close-tab.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ENInterfaces } from 'interfaces/en-interfaces.enum';
+import { FactoryONE } from 'src/app/classes/factory';
+import { transitionAnimation } from 'src/app/directives/animation.directive';
 
 @Component({
   selector: 'app-single-reading-counter',
   templateUrl: './single-reading-counter.component.html',
-  styleUrls: ['./single-reading-counter.component.scss']
+  styleUrls: ['./single-reading-counter.component.scss'],
+  animations: [transitionAnimation]
 })
-export class SingleReadingCounterComponent implements OnInit {
+export class SingleReadingCounterComponent extends FactoryONE {
   _searchByInfo: string = 'اشتراک';
 
   constructor(
     public closeTabService: CloseTabService,
     private offlineModeService: OfflineModeService
-  ) { }
+  ) {
+    super();
+  }
 
   connectToServer = async () => {
     if (this.offlineModeService.vertificationSingleReadingRequest(this.closeTabService.offlineSingleReadingCounterReq)) {
       this.closeTabService.offlineSingleReadingCounter = await this.offlineModeService.postDataSource(ENInterfaces.offlineSingleReadingCounter, this.closeTabService.offlineSingleReadingCounterReq);
       console.log(this.closeTabService.offlineSingleReadingCounter);
-      
+
     }
   }
-  ngOnInit(): void {
+  classWrapper = async (canRefresh?: boolean) => {
+    console.log(1);
+
+    this.closeTabService.offlineSingleReadingCounterReq.searchType = this.offlineModeService.getSearchTypes();
   }
 
 }
