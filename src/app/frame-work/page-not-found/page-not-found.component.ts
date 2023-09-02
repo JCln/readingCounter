95b7dd2200c8c1aca303670cb5b7e83a8071a1eb
JCln <ghmantera@gmail.com>
2023-09-02 23:20:49 +0430

    page not found refactored

--- src/app/frame-work/page-not-found/page-not-found.component.ts
+++ src/app/frame-work/page-not-found/page-not-found.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { EN_Routes } from 'interfaces/routes.enum';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class PageNotFoundComponent {
+  EN_Routes = EN_Routes;
 
 }
