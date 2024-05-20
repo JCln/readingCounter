a0a54ee39e6f5b41da62ac8a2e91d5cf93090ad6
JCln <ghmantera@gmail.com>
2024-05-20 12:58:56 +0330

    14030231

--- src/app/frame-work/branch-manager/request-draft/request-draft.component.ts
+++ src/app/frame-work/branch-manager/request-draft/request-draft.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
+import { IDictionaryManager } from 'interfaces/ioverall-config';
+import { MenuItem } from 'primeng/api';
+import { BranchesService } from 'services/branches.service';
+import { CloseTabService } from 'services/close-tab.service';
+import { FactoryONE } from 'src/app/classes/factory';
 
 @Component({
   selector: 'app-request-draft',
   templateUrl: './request-draft.component.html',
   styleUrls: ['./request-draft.component.scss']
 })
-export class RequestDraftComponent {
+export class RequestDraftComponent extends FactoryONE {
+  private readonly _outputFileName: string = 'requestDraftAdd';
+  _selectCols: any = [];
+  _selectColsAccordion: any = [];
+  items: MenuItem[];
+
+  qotrDictionary: IDictionaryManager[] = [];
+  zoneDictionary: IDictionaryManager[] = [];
+  usageDictionary: IDictionaryManager[] = [];
+  branchDiameterDictionary: IDictionaryManager[] = [];
+  guildDictionary: IDictionaryManager[] = [];
+  ownershipTypeDictionary: IDictionaryManager[] = [];
+  branchStateDictionary: IDictionaryManager[] = [];
+  waterSourceDictionary: IDictionaryManager[] = [];
+  customerTypeDictionary: IDictionaryManager[] = [];
+
+  constructor(
+    public closeTabService: CloseTabService,
+    public branchesService: BranchesService
+  ) {
+    super();
+  }
+  insertSelectedColumns = () => {
+    this._selectCols = this.branchesService.columnManager.getColumnsMenus(this._outputFileName);
+  }
+  dictionaryWrapper = async () => {
+    this.zoneDictionary = await this.branchesService.dictionaryWrapperService.getZoneDictionary();
+    this.usageDictionary = await this.branchesService.dictionaryWrapperService.getkarbariCodeDictionary();
+    this.branchDiameterDictionary = await this.branchesService.dictionaryWrapperService.getQotrDictionary();
+    this.guildDictionary = await this.branchesService.dictionaryWrapperService.getGuildDictionary(false);
+    this.ownershipTypeDictionary = await this.branchesService.dictionaryWrapperService.getOwnershipTypeDictionary(false);
+    this.branchStateDictionary = await this.branchesService.dictionaryWrapperService.getBranchStateDictionary(false);
+    this.waterSourceDictionary = await this.branchesService.dictionaryWrapperService.getWaterSourceDictionary(false);
+    this.customerTypeDictionary = await this.branchesService.dictionaryWrapperService.getCustomerTypeDictionary(false);
+  }
+  addStepperItems(): void {
+    this.items = [
+      {
+        label: 'راهنما',// توضیح اضافه برای عنوان اضافه بشه در افزودن کاربر
+        routerLink: 'desc'
+      },
+      {
+        label: 'اطلاعات شخصی',
+        routerLink: 'personal'
+      },
+      {
+        label: 'اطلاعات فنی',
+        routerLink: 'technical'
+      },
+      {
+        label: 'سایر اطلاعات',
+        routerLink: 'others'
+      },
+      {
+        label: 'اطلاعات مکانی',
+        routerLink: 'location'
+      },
+      {
+        label: 'تایید',
+        routerLink: 'confirmation'
+      }
+    ];
+  }
+  classWrapper = async () => {
+    this.addStepperItems();
+  }
 
 }
